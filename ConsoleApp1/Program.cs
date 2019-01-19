c8fdcf0aa00e31e6647d4e623831a44e00b93016
除 祓 <kaifengzhou@outlook.com>
2019-01-19 21:59:14 +0800

    添加初步的推荐算法。

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -20,10 +20,15 @@ namespace ConsoleApp1
             foreach (var node in nodes)
             {
                 string bookHerf = node.Attributes["href"].Value;
-                var bookHtml = @"http://book.sfacg.com/Novel/";
-                var bookWeb = new HtmlWeb();
-                var bookHtmlDoc = bookWeb.Load(bookHtml);
-                var bookNodes = bookHtmlDoc.DocumentNode.SelectNodes("");
+                string bID = bookHerf.Split('/')[2];
+                Console.WriteLine(bID);
+                books.Add(new Book(Convert.ToInt32(bID)));
+                //books[0].GetValues(bookHerf);
+            }
+            foreach (var eachBook in books)
+            {
+                Console.WriteLine(eachBook.bookID);
+                //eachBook.GetValues()
             }
             //Console.WriteLine(node.InnerHtml);
             Console.ReadKey();
@@ -31,47 +36,67 @@ namespace ConsoleApp1
     }
     struct Book
     {
-        public uint bookID;
+        public int bookID;
         public string title;
-        public uint words;
-        public DateTime updateTime;
-        public Array tags;
-        public uint pv;//click number
-        public uint likesNum;
-        public uint collectionNum;
-        public uint passNum;
-        public short score;
-        public string coverUrl;
-        public string author;
-        public string subject;//theme
-        public string description;
+        //public uint words;
+        //public DateTime updateTime;
+        //public Array tags;
+        //public uint pv;//click number
+        public int likesNum;
+        public int collectionNum;
+        //public uint passNum;
+        //public short score;
+        //public string coverUrl;
+        //public string author;
+        //public string subject;//theme
+        //public string description;
+        public double rank;
 
-        public void GetValues(string bookHerf)
+        public Book(int bid)
         {
-            var bookHtml = @"http://book.sfacg.com"+ bookHerf;
-            var bookWeb = new HtmlWeb();
-            var bookHtmlDoc = bookWeb.Load(bookHtml);
-            var bookNodes = bookHtmlDoc.DocumentNode.SelectNodes("");
+            string bookHtml = @"http://book.sfacg.com/Novel/" + bid;
+            Console.WriteLine(bookHtml);
+            HtmlWeb bookWeb = new HtmlWeb();
+            HtmlDocument bookHtmlDoc = bookWeb.Load(bookHtml);
+            title = bookHtmlDoc.DocumentNode.SelectSingleNode("//h1[@class='title']/span").InnerHtml;
+            bookID = bid;
+            //this.words = Convert.ToUInt32(bookHtmlDoc.DocumentNode.SelectSingleNode("//p[@class='introduce']").InnerHtml,16);
+            likesNum = Convert.ToInt32(bookHtmlDoc.DocumentNode.SelectSingleNode("//*[@id='BasicOperation']/a[2]").InnerHtml.Split()[1]);
+            collectionNum = Convert.ToInt32(bookHtmlDoc.DocumentNode.SelectSingleNode("//*[@id='BasicOperation']/a[3]").InnerHtml.Split()[1]);
+            if (collectionNum < 500)
+            {
+                rank = (Convert.ToDouble(likesNum) * Convert.ToDouble(collectionNum)) / (500*Convert.ToDouble(collectionNum));
+            }
+            else
+            {
+                rank = Convert.ToDouble(likesNum) / Convert.ToDouble(collectionNum);
+            }
+            Console.WriteLine(title);
+            Console.WriteLine(likesNum);
+            Console.WriteLine(collectionNum);
+            Console.WriteLine(string.Format("{0:0.00%}", rank));
+            Console.WriteLine();
+            //string this.title = bookNode.
         }
         
-        public void GetValues(uint bookID, string title, uint words, DateTime updateTime, 
-            Array tags, uint pv, uint likesNum, uint collectionNum, 
-            uint passNum, short score, string coverUrl, string author, string subject, string description)
+        public void GetValues(int bookID, string title, int words, DateTime updateTime, 
+            Array tags, int pv, int likesNum, int collectionNum, 
+            int passNum, short score, string coverUrl, string author, string subject, string description)
         {
             this.bookID = bookID;
             this.title = title;
-            this.words = words;
-            this.updateTime = updateTime;
-            this.tags = tags;
-            this.pv = pv;
+            //this.introduce = words;
+            //this.updateTime = updateTime;
+            //this.tags = tags;
+            //this.pv = pv;
             this.likesNum = likesNum;
             this.collectionNum = collectionNum;
-            this.passNum = passNum;
-            this.score = score;
-            this.coverUrl = coverUrl;
-            this.author = author;
-            this.subject = subject;
-            this.description = description;
+            //this.passNum = passNum;
+            //this.score = score;
+            //this.coverUrl = coverUrl;
+            //this.author = author;
+            //this.subject = subject;
+            //this.description = description;
         }
         public void BookDisplay()
         {
